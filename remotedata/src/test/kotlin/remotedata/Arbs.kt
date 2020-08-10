9175ee8dca201ebfdca0154b8c57371c13b03b5d
Michael Torres <torresmi@mail.gvsu.edu>
2020-08-10 03:12:15 -0400

    replace spek with kotest

---
+++ remotedata/src/test/kotlin/remotedata/Arbs.kt
@@ -0,0 +1,8 @@
+package remotedata
+
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.arb
+
+fun <A> Arb<A>.plusEdgecases(vararg edgecases: A): Arb<A> = arb(this.edgecases().plus(edgecases)) { rs ->
+    generate(rs).map { it.value }
+}
