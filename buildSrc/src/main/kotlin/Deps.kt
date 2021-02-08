0671f6a720af00f177bdf32c7866dc346d65c5a3
Micahel Torres <torresmi@mail.gvsu.edu>
2021-02-07 22:54:22 -0500

    update dependencies

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -34,11 +34,11 @@ private fun dependency(path: String, version: String) = "$path:$version"
 
 object Versions {
     val bintray = "1.8.5"
-    val coroutines = "1.4.0"
+    val coroutines = "1.4.2"
     val dokka = "1.4.10.2"
-    val jacoco = "0.8.5"
-    val kotest = "4.2.4"
-    val kotlin = "1.4.10"
+    val jacoco = "0.8.6"
+    val kotest = "4.4.0"
+    val kotlin = "1.4.30"
     val maven = "1.4.1"
-    val versions = "0.33.0"
+    val versions = "0.36.0"
 }
