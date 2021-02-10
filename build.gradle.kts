9043ddf3c4d8715b4936bb9d507d52bd290cd9ae
Micahel Torres <torresmi@mail.gvsu.edu>
2021-02-09 23:59:11 -0500

    use jcenter temporarily for dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ buildscript {
 
     repositories {
         mavenCentral()
+        jcenter()
     }
 
     dependencies {
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.10.2")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.33.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.13.0")
@@ -21,6 +22,7 @@ subprojects {
 
     repositories {
         mavenCentral()
+        jcenter()
     }
 }
 
@@ -28,6 +30,11 @@ subprojects {
 
 repositories {
     mavenCentral()
+    jcenter()
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>().configureEach {
+    outputDirectory.set(projectDir.resolve("docs"))
 }
 
 apply(plugin="org.jetbrains.dokka")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven(url="https://dl.bintray.com/kotlin/dokka")
+    }
+}
+
 plugins {
     id("com.pablisco.gradle.automodule") version "0.15"
 }
